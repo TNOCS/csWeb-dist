5cc793b593dff9a341143eb6d791b455e4e5ebb2
Erik Vullings <erik.vullings@gmail.com>
2015-11-13 16:41:11 +0100

    Added npm release

---
+++ dist-npm/ServerComponents/api/RestAPI.d.ts
@@ -0,0 +1,16 @@
+import ApiManager = require('./ApiManager');
+import express = require('express');
+import BaseConnector = require('./BaseConnector');
+export declare class RestAPI extends BaseConnector.BaseConnector {
+    server: express.Express;
+    baseUrl: string;
+    manager: ApiManager.ApiManager;
+    resourceUrl: any;
+    layersUrl: any;
+    keysUrl: any;
+    filesUrl: any;
+    searchUrl: any;
+    projectsUrl: any;
+    constructor(server: express.Express, baseUrl?: string);
+    init(layerManager: ApiManager.ApiManager, options: any, callback: Function): void;
+}
---
+++ dist-npm/csServer.d.ts
@@ -0,0 +1,16 @@
+import express = require('express');
+import csweb = require('./index');
+export declare class csServerOptions {
+    port: number;
+}
+export declare class csServer {
+    dir: string;
+    options: csServerOptions;
+    server: express.Express;
+    cm: csweb.ConnectionManager;
+    messageBus: csweb.MessageBusService;
+    httpServer: any;
+    config: csweb.ConfigurationService;
+    constructor(dir: string, options?: csServerOptions);
+    start(started: Function): void;
+}
