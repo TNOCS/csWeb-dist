620d4e62c7c1613aa1eb8fe26e39375d8f8e23d9
Erik Vullings <erik.vullings@gmail.com>
2015-12-02 16:45:34 +0100

    Also xcopy subdirectories

--- release.bat
+++ release.bat
@@ -1,7 +1,7 @@
 REM @echo off
 REM Useage: release major | minor | patch
-xcopy ..\csWeb\dist-bower\*.* dist-bower\ /Y
-xcopy ..\csWeb\dist-npm\*.*   dist-npm\   /Y
+xcopy ..\csWeb\dist-bower\*.* dist-bower\ /Y /S
+xcopy ..\csWeb\dist-npm\*.*   dist-npm\   /Y /S
 bower version %1
 REM Do not try to create a new git tag, as bower already did this.
 npm version --no-git-tag-version %1
